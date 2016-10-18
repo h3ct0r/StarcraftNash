914c83514ea55e6a741cb284c189a9e79c5d28ef
anderson <andersonrochatavares@gmail.com>
2016-10-18 14:37:42 -0200

    exp3 now updates weights before selecting

--- strategies/exp3.py
+++ strategies/exp3.py
@@ -29,13 +29,13 @@ class Exp3(StrategyBase):
     The method is from the paper:
     Gambling in a rigged casino: the adversarial multi-armed bandit problem
 
-    This implementation is an adaption of:
+    This implementation is an adaptation of:
     https://github.com/johnmyleswhite/BanditsBook/blob/master/python/algorithms/exp3/exp3.py
     """
 
     def __init__(self):
         """
-        Initializes epsilon-greedy strategy selection method
+        Initializes strategy selection method
         """
 
         StrategyBase.__init__(self)
@@ -48,6 +48,11 @@ class Exp3(StrategyBase):
         Selects a bot according to the Exp3 algorithm
         :return:
         """
+        # updates weights with result from previous match
+        if self.my_choice(-1) is not None:  # skips on first match
+            self.update(self.my_choice(-1), self.match_result(-1))
+
+        # performs selection
         n_arms = len(self.weights)
         total_weight = sum(self.weights.values())
 
@@ -60,16 +65,25 @@ class Exp3(StrategyBase):
         return categorical_draw(probs)
 
     def update(self, chosen_arm, reward):
+        """
+        Updates the weight of the chosen arm according to the received reward
+        :param chosen_arm:
+        :param reward:
+        :return:
+        """
         n_arms = len(self.weights)
         total_weight = sum(self.weights.values())
 
-        probs = {choice: 0.0 for choice in self.bot_list}
+        #probs = {choice: 0.0 for choice in self.bot_list}
 
-        for arm in self.bot_list:
-            probs[arm] = (1 - self.gamma) * (self.weights[arm] / total_weight)
-            probs[arm] += self.gamma * (1.0 / float(n_arms))
+        #for arm in self.bot_list:
+        #    probs[arm] = (1 - self.gamma) * (self.weights[arm] / total_weight)
+        #    probs[arm] += self.gamma * (1.0 / float(n_arms))
 
-        x = reward / probs[chosen_arm]
+        prob_of_chosen_arm = (1 - self.gamma) * (self.weights[chosen_arm] / total_weight) \
+                             + self.gamma * (1.0 / float(n_arms))
+
+        x = reward / prob_of_chosen_arm  # probs[chosen_arm]
 
         growth_factor = math.exp((self.gamma / n_arms) * x)
 
